8188846ecca88589d334be43306f394d5d84c1f7
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-02-10 20:42:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(401,437);
+canvas.center();
+}
+
+function draw(){
+    
+}
+
+function takesnapshot(){
+    save('example.png');
+}
