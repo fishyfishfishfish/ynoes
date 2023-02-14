f1c01499fe70807108b9896bb792e60cf5e56d4e
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-02-13 20:35:58 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-function preload(){
+function preload() {
 
 }
 
-function setup(){
-canvas=createCanvas(401,437);
-canvas.center();
+function setup() {
+    canvas = createCanvas(401, 437);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(401, 437);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("posenet is alive and not dead (;");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log(nosex);
+        console.log(nosey);
+    }
 }
 
-function draw(){
-    
+
+
+function draw() {
+    image(video, 0, 0, 401, 437);
 }
 
-function takesnapshot(){
+function takesnapshot() {
     save('example.png');
 }
